54875050b1afb18c78f9b6d4f0bc11944628c32c
KxNick <kxnick@gmail.com>
2025-12-24 16:56:53 +0800

    Add chapter example

---
+++ example-01/example-1-2-simple.c
@@ -0,0 +1,36 @@
+#include <gtk/gtk.h>
+
+static void on_button_clicked(GtkButton *btn, gpointer data) {
+    g_print("Button pressed!\n");
+}
+
+static void activate(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window;
+    GtkWidget *button;
+
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello Button GTK3");
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+
+    button = gtk_button_new_with_label("Press Me");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+
+    gtk_container_add(GTK_CONTAINER(window), button);
+
+    gtk_widget_show_all(window);
+}
+
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("org.example.hello", G_APPLICATION_FLAGS_NONE);
+
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+
+    g_object_unref(app);
+
+    return status;
+}
